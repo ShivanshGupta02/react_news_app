005550bf07a9de802aacf0eadd3ece6537599fcb
Shivansh Gupta <sgupta121202@gmail.com>
2023-12-08 20:07:25 +0530

    committed

--- client/src/firebase/setup.js
+++ client/src/firebase/setup.js
@@ -1,15 +1,20 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
-const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const VITE_FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const VITE_AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const VITE_PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+const VITE_STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+const VITE_APP_ID = import.meta.env.VITE_APP_ID;
+const VITE_MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: "news-app-64f70.firebaseapp.com",
-  projectId: "news-app-64f70",
-  storageBucket: "news-app-64f70.appspot.com",
-  messagingSenderId: "299517470156",
-  appId: "1:299517470156:web:94550281d864a80e6ea969"
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID
 };
 
 // Initialize Firebase
